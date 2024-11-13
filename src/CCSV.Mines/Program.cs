8874e9598e69d782801bc5c168419adc6d8c2dd3
Stefano Vidaurre Olite <fano494@gmail.com>
2024-11-14 00:13:37 +0100

    feat: add raylib

--- src/CCSV.Mines/Program.cs
+++ src/CCSV.Mines/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace CCSV.Mines;
+﻿using Raylib_cs;
+
+namespace CCSV.Mines;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
     }
 }
